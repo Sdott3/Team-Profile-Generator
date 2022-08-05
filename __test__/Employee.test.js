c76fec6239b2dfc505abba9ec41e2468221a13cf
sdott3 <sdotthomas347@gmail.com>
2022-08-05 15:06:30 -0400

    created first failed test to create employee object

---
+++ __test__/Employee.test.js
@@ -0,0 +1,21 @@
+// The first class is an Employee parent class with the following properties and methods:
+
+
+// name
+// id
+// email
+// getName()
+// getId()
+// getEmail()
+// getRole() // Returns 'Employee'
+
+const Employee = require('../lib/Employee.js');
+
+// test to create Employee object
+test('creates an employee', () => {
+    const employee = new Employee('Sha Thomas', 3, 'sdotthomas347@gmail.com');
+
+    expect(employee.name).toBe('Sha Thomas');
+    expect(employee.id).toBe('3');
+    expect(employee.email).toBe('sdotthomas347@gmail.com');
+});
