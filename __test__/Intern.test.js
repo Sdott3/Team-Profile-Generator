b9f6462970a6a510b68365a01d2fbb3057d70555
sdott3 <sdotthomas347@gmail.com>
2022-08-05 17:56:48 -0400

    3 out of 4 intern test pass

--- __test__/Intern.test.js
+++ __test__/Intern.test.js
@@ -1,5 +1,16 @@
 // Intern parent class with the following properties and methods:
 const Intern = require('../lib/Intern.js');
 // school
+test('create a intern object', () => {
+    const intern = new Intern('Shaquille Sunflower', 60, 'ssunflower@gmail.c0m', 'MSU');
+
+    expect(intern.school).toEqual(expect.any(String));
+});
+
 // getSchool()
+test('to get intern School', () => {
+    const intern = new Intern('Shaquille Sunflower', 60, 'ssunflower@gmail.c0m', 'MSU');
+
+    expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
+});
 // getRole() // Overridden to return 'Intern'
